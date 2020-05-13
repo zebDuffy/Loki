4bf0511f91f4078ff5c3f7ff4997362097adf1be
zebDuffy <billduffy@gmail.com>
2020-05-12 22:40:53 -0400

    fix notification

--- app/src/main/java/com/swamptown/bill/loki/DownloadNotification.java
+++ app/src/main/java/com/swamptown/bill/loki/DownloadNotification.java
@@ -8,19 +8,18 @@ import android.content.Intent;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.TaskStackBuilder;
 
-
 /**
  * Created by bill on 7/28/16.
  */
 public class DownloadNotification {
-    public void DoNotification(String title, String notifciation, Context context )
+    public void DoNotification(String title, String notification, Context context )
     {
         int mId=1;
         NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(context)
+                new NotificationCompat.Builder(context, "1")
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle(title)
-                        .setContentText(notifciation);
+                        .setContentText(notification);
         Intent resultIntent = new Intent(context, MainActivity.class);
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
         stackBuilder.addParentStack(MainActivity.class);
--- app/src/main/java/com/swamptown/bill/loki/MainActivity.java
+++ app/src/main/java/com/swamptown/bill/loki/MainActivity.java
@@ -1,10 +1,13 @@
 package com.swamptown.bill.loki;
 
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.WallpaperManager;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -28,6 +31,7 @@ import java.util.Random;
 
 public class MainActivity extends
         AppCompatActivity {
+    public static final String CHANNEL_ID ="1" ;
     public static String lokiPrefs="Loli.cfg";
     public Context context;
 
@@ -78,6 +82,8 @@ public class MainActivity extends
                     break;
                 }
             }
+            createNotificationChannel();
+
         }
 
         downloadButton.setOnClickListener(new View.OnClickListener() {
@@ -183,6 +189,21 @@ public class MainActivity extends
 
             return super.onOptionsItemSelected(item);
         }
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 
 
     }
